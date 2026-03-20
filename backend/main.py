c465ff3f62168fafca3f1800ceddde36a65fbac0
anin <aanindya05@gmail.com>
2026-03-20 16:56:39 +0700

    feat(api): add PUSH and DELETE endpoints

--- backend/main.py
+++ backend/main.py
@@ -23,3 +23,21 @@ def get_mahasiswa():
 def create_mahasiswa(mhs: Mahasiswa):
     db_mahasiswa.append(mhs.model_dump())
     return {"pesan": "Data mahasiswa baru berhasil ditambahkan!", "data": mhs}
+
+# Endpoint PUT (Untuk memperbarui data mahasiswa)
+@app.put("/mahasiswa/{id}")
+def update_mahasiswa(id: str, mhs: Mahasiswa):
+    for i, data in enumerate(db_mahasiswa):
+        if data["id"] == id:
+            db_mahasiswa[i] = mhs.model_dump()
+            return {"pesan": f"Data mahasiswa dengan ID {id} berhasil diperbarui!", "data": mhs}
+    return {"pesan": f"Mahasiswa dengan ID {id} tidak ditemukan"}
+
+# Endpoint DELETE (Untuk menghapus data mahasiswa)
+@app.delete("/mahasiswa/{id}")
+def delete_mahasiswa(id: str):
+    for i, data in enumerate(db_mahasiswa):
+        if data["id"] == id:
+            hapus = db_mahasiswa.pop(i)
+            return {"pesan": f"Data mahasiswa dengan ID {id} berhasil dihapus!", "data": hapus}
+    return {"pesan": f"Mahasiswa dengan ID {id} tidak ditemukan"}
