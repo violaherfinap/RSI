3a23812250bbcf6d4b41770395c84f255a05de8e
T-Rex2586 <theodosiusmahardika9@mail.com>
2026-03-21 12:05:05 +0700

    fix(backend): tambah CORS agar frontend dapat mengakses API

--- backend/main.py
+++ backend/main.py
@@ -1,30 +1,34 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# Kerangka data khusus untuk Mahasiswa
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 class Mahasiswa(BaseModel):
     id: str
     nama: str
     jurusan: str
     fakultas: str
 
-# Database bodong
 db_mahasiswa = []
 
-# Endpoint GET (Untuk melihat data mahasiswa)
 @app.get("/mahasiswa")
 def get_mahasiswa():
     return {"pesan": "Ini adalah daftar mahasiswa", "data": db_mahasiswa}
 
-# Endpoint POST (Untuk menambah data mahasiswa baru)
 @app.post("/mahasiswa")
 def create_mahasiswa(mhs: Mahasiswa):
     db_mahasiswa.append(mhs.model_dump())
     return {"pesan": "Data mahasiswa baru berhasil ditambahkan!", "data": mhs}
 
-# Endpoint PUT (Untuk memperbarui data mahasiswa)
 @app.put("/mahasiswa/{id}")
 def update_mahasiswa(id: str, mhs: Mahasiswa):
     for i, data in enumerate(db_mahasiswa):
@@ -33,7 +37,6 @@ def update_mahasiswa(id: str, mhs: Mahasiswa):
             return {"pesan": f"Data mahasiswa dengan ID {id} berhasil diperbarui!", "data": mhs}
     return {"pesan": f"Mahasiswa dengan ID {id} tidak ditemukan"}
 
-# Endpoint DELETE (Untuk menghapus data mahasiswa)
 @app.delete("/mahasiswa/{id}")
 def delete_mahasiswa(id: str):
     for i, data in enumerate(db_mahasiswa):
